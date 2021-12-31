e5d1891041bd001768bc2b6984b75e298348cb30
sujan-sht <sujanstha016@gmail.com>
2021-12-31 16:28:59 +0545

    app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,29 +1,39 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
-  /**
-   * Bootstrap any application services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    Schema::defaultStringLength(191);
-  }
-
-  /**
-   * Register any application services.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    //
-  }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->removePublicFromURl();
+        if(env('APP_ENV') != 'local'){
+            URL::forceScheme('https');
+        }
+    }
+    protected function removePublicFromURl()
+    {
+        if (Str::contains(request()->getRequestUri(), '/public/')) {
+            $url = str_replace('public/', '', request()->getRequestUri());
+            if (strlen($url) > 0) {
+                header("Location: $url", true, 301);
+                exit;
+            }
+        }
+    }
 }
